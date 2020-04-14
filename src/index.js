e3e0a1fa8d6db4a2cf9bfff99e6dc1cedf211c84
JenifferGenoves <jenifer.genoves03@gmail.com>
2020-04-13 22:48:58 -0400

    segundo commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,137 @@
-import validator from './validator.js';
+// import validator from './validator.js';
 
-console.log(validator);
+// console.log(validator);
+
+// const validator = {
+//   // ...
+// };
+
+// export default validator;
+
+//DECL FUNCION GENERICA
+function ocultarYMostrar(ocultarID, mostrarID) {
+    document.getElementById(ocultarID).classList.add('oculto');
+    document.getElementById(mostrarID).classList.remove('oculto');
+  }
+  
+  //--------De bienvenida a formulario registro------------//
+  function seleccionarRegistro() {
+    ocultarYMostrar('bienvenido', 'registroUsuario');
+  }
+  
+  //----De registro a formulario validación tarjeta---//
+  function irFormularioTarjeta() {
+    ocultarYMostrar('registroUsuario', 'formulario-tarjeta');
+  }
+  
+  function cancelFormularioTarjeta() {
+    ocultarYMostrar('registroUsuario', 'bienvenido');
+  }
+  
+  //----De validacion tarjeta a OK, reintentar o cancelar, luego del PROMPT o alert correspondiente----//
+  function okTarjForm() {
+    ocultarYMostrar('formulario-tarjeta', 'bienvenido');
+  }
+  
+  function reintTarjForm() {
+    ocultarYMostrar('formulario-tarjeta', 'registroUsuario'); //acà deberìan limpiarse los campos//
+  }
+  
+  function cancelTarjForm() {
+    ocultarYMostrar('formulario-tarjeta', 'registroUsuario');
+  }
+  
+  function testvalidation() {
+    const num = document.getElementById('inputNumero').value;
+    valid_credit_card(num);
+  }
+  
+  //TARJETA INTERACTIVA-----//
+  
+  // const tarjeta = document.querySelector('#tarjeta');
+  // btnAbrirFormulario = document.querySelector('#btn-abrir-formulario');
+  // formulario = document.querySelector('#formulario-tarjeta');
+  // numeroTarjeta = document.querySelector('#tarjeta .numero');
+  // nombreTarjeta = document.querySelector('#tarjeta .nombre');
+  // logoMarca = document.querySelector('#logo-marca');
+  // firma = document.querySelector('#tarjeta .firma p');
+  // mesExpiracion = document.querySelector('#tarjeta .mes');
+  // yearExpiracion = document.querySelector('#tarjeta .year');
+  // ccv = document.querySelector('#tarjeta .ccv');
+  
+  //formulario tarjeta con funcion//
+  
+  // // * Input nombre de tarjeta
+  // formulario.inputNombre.addEventListener('keyup', (e) => {
+  //   let valorInput = e.target.value;
+  
+  //   formulario.inputNombre.value = valorInput.replace(/[0-9]/g, '');
+  //   nombreTarjeta.textContent = valorInput;
+  //   firma.textContent = valorInput;
+  
+  //   if (valorInput == '') {
+  //     nombreTarjeta.textContent = 'Juan Pérez';
+  //   }
+  
+  //   mostrarFrente();
+  // });
+  
+  // // * Select mes
+  // formulario.selectMes.addEventListener('change', (e) => {
+  //   mesExpiracion.textContent = e.target.value;
+  //   mostrarFrente();
+  // });
+  
+  // // * Select Año
+  // formulario.selectYear.addEventListener('change', (e) => {
+  //   yearExpiracion.textContent = e.target.value.slice(2);
+  //   mostrarFrente();
+  // });
+  
+  // // * CCV
+  // formulario.inputCCV.addEventListener('keyup', () => {
+  //   if (!tarjeta.classList.contains('active')) {
+  //     tarjeta.classList.toggle('active');
+  //   }
+  
+  //   formulario.inputCCV.value = formulario.inputCCV.value
+  //     // Eliminar los espacios
+  //     .replace(/\s/g, '')
+  //     // Eliminar las letras
+  //     .replace(/\D/g, '');
+  
+  //   ccv.textContent = formulario.inputCCV.value;
+  // });
+  
+  
+  
+  
+  
+  //   import validator from './validator.js';
+  
+  // document.getElementById("validar").addEventListener("click", validator.isValid, false);
+  
+  
+  // document.getElementById("contenedor1").style.display = "";
+  // document.getElementById("contenedor2").style.display = "none";
+  
+  
+  
+  
+  
+  
+  // //aqui van todas las funciones que me permiten el cambio de pantalla 
+  // let capturar = () => {
+  
+  //   document.getElementById("contenedor1").style.display = "none";
+  //   document.getElementById("contenedor2").style.display = "";
+  
+  // }
+  
+  // document.getElementById("botonenviar").addEventListener("click", capturar , false);
+  
+  
+  
+  
+  
+  
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,134 @@
-const validator = {
-  // ...
-};
+// const validator = {
+//   // ...
+// };
 
-export default validator;
+// export default validator;
+
+// / const validator = {
+  //   // ...
+  // };
+  
+  // export default validator;
+  // Takes a credit card string value and returns true on valid number
+  function valid_credit_card(value) {
+    // Accept only digits, dashes or spaces
+    if (/[^0-9-\s]+/.test(value)) return false;
+  
+    // The Luhn Algorithm. It's so pretty.
+    let nCheck = 0,
+      bEven = false;
+    value = value.replace(/\D/g, '');
+  
+    for (var n = value.length - 1; n >= 0; n--) {
+      var cDigit = value.charAt(n),
+        nDigit = parseInt(cDigit, 10);
+  
+      if (bEven && (nDigit *= 2) > 9) nDigit -= 9;
+  
+      nCheck += nDigit;
+      bEven = !bEven;
+    }
+  
+    return alert(nCheck % 10 == 0);
+  }
+  
+  
+  
+  
+  
+  const valid_credit_card = () => {
+      a = 'valid_credit_card'
+      return a;
+  }
+  
+  export default valid_credit_card;
+  
+  
+  //const valorInput 
+  
+  // const validator = num => {
+  //    valorInput = (num + '')
+  //    .split('')
+  //    .reverse()
+  //    .map(x => parseInt (x));
+  //    let lastDigit = array.splice(0, 1) [0];
+  //    let sum = arr.reduce((acc, val, i) => (i % 2 !== 0 ? acc + val : acc + ((val * 2) % 9) || 9), 0);
+  //    sum += lastDigit;
+  //    return sum % 10 === 0;
+  //  };
+  // //  console.log (validator);
+  
+  // export default validator;
+  
+  
+  // const validator = {
+  //   //let mascara = maskify(texto);
+  //   console.log("Este es el numero de tarjeta enmascarada "+ mascara);
+  //   //document.write("Tu numero de tarjeta de credito termina en " + mascara);  
+  //   return mascara;
+  // },
+  
+    // return validator; 
+  
+    //import maskify from 'maskify';
+  
+  // const validator = {
+  
+  //   maskify : function () {
+  
+  //       let texto = document.getElementById("tdc").value;
+  //       let mascara = "############"+ texto.substring(12);
+  //       //let mascara = maskify(texto);
+  //       console.log("Este es el numero de tarjeta enmascarada "+ mascara);
+  //       //document.write("Tu numero de tarjeta de credito termina en " + mascara);  
+  //       return mascara;
+  //   },
+    
+  //   isValid : function () {
+  //       let texto = document.getElementById("tdc").value;
+  //       if (texto == "" || texto.length < 16) {
+  //         alert("El Campo TDC esta incompleto");
+  //         return;
+  //       } else {
+  //         let cantidadCaracteres = texto.length;
+  //         console.log("Numero de caracteres " + cantidadCaracteres);
+  //         console.log("Texto de la Cadena " + texto);
+  //         let sum = 0;
+  //         for (let i = cantidadCaracteres - 1; i >= 0; i--) {
+  //           let ent = parseInt(texto.charAt(i));
+  //           console.log("Entero inicial es " + ent);
+  //           console.log("El indice es " + i);
+  //           if (i % 2 == 0) {
+  //               ent = ent * 2;
+  //               console.log("Es par el indice, ent * 2 es igual a " + ent);
+    
+  //               if (ent > 9) {
+  //                   console.log("si ent es > que 9 entonces el valor es " + ent);
+  //                   ent = ent - 9;
+  //                   console.log("Ahora ent - 9 es igual a " + ent);
+  //               }
+  //           }
+    
+  //           sum = sum + ent;
+  //           console.log(sum);
+  //         }
+  //         let residuo = sum % 10;
+  //         console.log("El residuo es " + residuo);
+  //         if (residuo == 0) {
+  //           console.log("Tarjeta Valida");
+  //           validator.maskify();
+  //           return true;
+  //         } else {
+  //           console.log("Tarjeta InValida");
+  //           validator.maskify();
+  //           return false;
+  //         }
+    
+  //       }
+  //     }
+  
+  // };
+  
+  // export default validator;
+  
+  
